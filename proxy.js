4a3e9dea2759525ec4580891a7deb977f70033d4
Lomash Jangde <lomash265@gmail.com>
2026-02-26 00:47:48 +0530

    protected all api routes

--- proxy.js
+++ proxy.js
@@ -1,12 +1,18 @@
 import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server'
 
 const isDashboardRoute = createRouteMatcher(['/dashboard(.*)'])
+const isApiRoute = createRouteMatcher(['/api(.*)'])
 const isAuthRoute = createRouteMatcher(['/sign-in(.*)', '/sign-up(.*)', '/'])
 
 export default clerkMiddleware(async (auth, req) => {
   // If visiting /dashboard and not authenticated → redirect to sign-in
   if (isDashboardRoute(req)) {
     await auth.protect() // automatically redirects to sign-in if not signed in
+  }
+
+  // protecting api routes
+  if (isApiRoute(req)) {
+    await auth.protect()
   }
 
   // If visiting /sign-in or /sign-up and user **is** signed in → send to /dashboard
