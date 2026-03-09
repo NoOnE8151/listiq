7422c0921af23b0fd5a1973a74154b257df72549
Lomash Jangde <lomash265@gmail.com>
2026-03-09 22:28:29 +0530

    added shop ui to purchase credits

--- app/credits/page.jsx
+++ app/credits/page.jsx
@@ -1,24 +1,30 @@
 import React from "react";
 import CreditCard from "@/components/shop/CreditCard";
+import Link from "next/link";
 
 const CreditShop = () => {
   return (
     <>
-      <header className="md:px-16 md:py-5 flex items-center justify-between">
+      <header className="md:px-16 md:py-5 flex items-center justify-between px-5 py-3 shadow-xs">
         <div>
           <h1 className="text-2xl font-semibold">Buy Credits</h1>
         </div>
-        <div>
+        <Link href={'/dashboard'}>
           <h2 className="font-riot text-xl">
-            <span>List</span>
-            <span>IQ</span>
+            <span className="text-[#3FBBEB]">List</span>
+            <span className="text-element">IQ</span>
           </h2>
-        </div>
+        </Link>
       </header>
 
-      <main className="md:px-16 md:py-5">
-        <div className="border border-gray-100 grid grid-cols-3">
-            <CreditCard />
+      <main className="md:px-16 md:py-5 px-5 py-3">
+        <div className="grid grid-cols-1 md:grid-cols-3 gap-12 my-8">
+            <CreditCard amount={100} price={299} />
+            <CreditCard amount={200} price={499} />
+            <CreditCard amount={300} price={599} />
+            <CreditCard amount={400} price={699} />
+            <CreditCard amount={500} price={799} />
+            <CreditCard amount={600} price={899} />
         </div>
       </main>
     </>
--- components/shop/CreditCard.jsx
+++ components/shop/CreditCard.jsx
@@ -1,9 +1,51 @@
-import React from 'react'
+import React from "react";
 
-const CreditCard = () => {
+const CreditCard = ({ amount, price, highlight }) => {
   return (
-    <div>CreditCard</div>
-  )
-}
+    <div
+      className={`relative rounded-xl p-6 bg-background 
+      border border-gray-100 shadow-sm
+      transition-all duration-200
+      hover:shadow-xl hover:border-gray-100 cursor-pointer hover:-translate-y-1`}
+    >
+      {highlight && (
+        <span className="absolute top-3 right-3 text-xs px-2 py-1 rounded-md bg-element text-white">
+          Popular
+        </span>
+      )}
 
-export default CreditCard
+      {/* Top Section */}
+      <div className="flex items-center gap-3 mb-6">
+        <div className="bg-element/10 p-2 rounded-lg">
+          <img
+            src="/assets/elements/coin.svg"
+            alt="credit"
+            className="w-8 h-8"
+          />
+        </div>
+
+        <div>
+          <h2 className="text-xl font-semibold">{amount} Credits</h2>
+          <p className="text-xs opacity-60">Use across all tools</p>
+        </div>
+      </div>
+
+      {/* Bottom */}
+      <div className="flex items-end justify-between">
+        <div className="text-3xl font-bold">
+          ₹{price}
+        </div>
+
+        <button
+          className="bg-element px-4 py-2 w-1/3 text-white font-semibold rounded-lg text-sm cursor-pointer hover:bg-element-hover active:bg-element-active
+          transition-all duration-200
+          hover:brightness-110 active:scale-95"
+        >
+          Buy
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default CreditCard;  
